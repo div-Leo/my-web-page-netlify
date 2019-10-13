b955b18a8aa5a2145343fe44dcd5572187aad253
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-10-13 13:55:36 +0200

    feat: home page

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 
 import Header from "./header/index"
@@ -7,20 +7,20 @@ import DotCursor from "../components/dotCursor"
 import Transition from "../components/utils/transition"
 
 const Layout = ({ children, location, ...props }) => {
-  const isNight = new Date().getHours() < 8 || new Date().getHours() > 19;
-  // const isNight = true;
-  // const isNight = false;
-
+  
+  useEffect(() => {
+    const isNight = new Date().getHours() < 8 || new Date().getHours() > 19;
+    document.getElementsByTagName('html')[0].classList.add(isNight ? 'dark_theme' : 'light_theme')
+  },[])
+  
   return (
-    <div className={isNight ? 'dark_theme' : 'light_theme'}>
-      <div className='layout'>
-        <Header siteTitle={props['*'] || 'Portfolio'} page={props['*']} />
-          <Transition location={location} >
-              <main>{children}</main>
-          </Transition>
-        <Footer />
-        <DotCursor location={location}/>
-      </div>
+    <div>
+      <Header siteTitle={props['*'] || 'Portfolio'} page={props['*']} />
+        <Transition location={location} >
+            <main>{children}</main>
+        </Transition>
+      <Footer />
+      <DotCursor location={location}/>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React from "react";
 
-import SEO from "../components/utils/seo"
-import Glitch from "../components/glitch"
-import About from "../components/about"
+import SEO from "../components/utils/seo";
+import Glitch from "../components/glitch";
+import About from "../components/about";
+import Skills from "../components/skills";
+import Languages from '../components/languages';
+import Form from '../components/form';
 
 import '../styles/index.sass'
 
@@ -12,6 +15,9 @@ const IndexPage = () => {
       <SEO title="Portfolio" keywords={[`portfolio`, `frontend`, `developer`]} />
       <Glitch/>
       <About/>
+      <Skills/>
+      <Languages/>
+      <Form/>
     </>
   )
 }
