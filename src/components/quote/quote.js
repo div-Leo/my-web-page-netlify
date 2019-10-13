0265d89ac769a86f6d5d300a0691201b9ba6db89
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-10-13 13:53:03 +0200

    feat: quote

---
+++ src/components/quote/quote.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quote = () => (
+  <div className="quote_container">
+    <div className="quote">
+      Favorite joke: <br/><br/>
+      <i>"Two CSS properties walk into a bar.<br/><br/>A barstool in a completely different bar falls over."</i><br/><br/> 😂😂😂
+    </div>   
+  </div>   
+)
+
+export default Quote
