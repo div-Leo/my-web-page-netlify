ed05a744f38938b66f0f6ced3fae0936e73efb5a
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2021-01-08 16:59:06 +0100

    gif to img tag

--- src/components/languages/languages.js
+++ src/components/languages/languages.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Revealer from "../utils/revealer"
+import Revealer from "../utils/revealer";
+import italian from "../../images/italian.gif";
+import english from "../../images/english.gif";
+import spanish from "../../images/spanish.gif";
 
 const Languages = () => {
   return (
@@ -7,7 +10,19 @@ const Languages = () => {
       <Revealer revealIn="fadeInUp" revealOut="fadeOut">
         <div className="languages_gif">
           <div className="gif_container">
-            <div className="languages_image"></div>
+            <div className="languages_image">
+            <div className="gifs">
+              <div className="gif">
+                <img src={italian}></img>
+              </div>
+              <div className="gif">
+                <img src={english}></img>
+              </div>
+              <div className="gif">
+                <img src={spanish}></img>
+              </div>
+            </div>
+            </div>
             <div className="languages_icon">
             </div>
           </div>
